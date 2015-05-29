1d59c1ebe3ac995ad9ff2d43490e18a6c21a9555
Greg <gregloring@gmail.com>
2015-05-28 22:00:28 -0700

    added code

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@
 
 module.exports = function (grunt) {
 
-  grunt.loadNpmTasks('grunt-build-control');
+  var pkg = require('./package.json');
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
@@ -380,33 +380,35 @@ module.exports = function (grunt) {
         'svgmin'
       ]
     },
-    buildcontrol: {
-        options: {
-          dir: 'dist',
-          commit: true,
-          push: true,
-          message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-        },
-        pages: {
-          options: {
-            remote: 'https://github.com/Gregerl01/gregloring_2015_portfolio.git',
-            branch: 'gh-pages'
-          }
-        },
-        heroku: {
-          options: {
-            remote: 'git@heroku.com:example-heroku-webapp-1988.git',
-            branch: 'master',
-            tag: pkg.version
-          }
-        },
-        local: {
-          options: {
-            remote: '../',
-            branch: 'build'
-          }
-        }
+
+      buildcontrol: {
+    options: {
+      dir: 'dist',
+      commit: true,
+      push: true,
+      message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+    },
+    pages: {
+      options: {
+        remote: 'git@github.com:example_user/example_webapp.git',
+        branch: 'gh-pages'
       }
+    },
+    heroku: {
+      options: {
+        remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+        branch: 'master',
+        tag: pkg.version
+      }
+    },
+    local: {
+      options: {
+        remote: '../',
+        branch: 'build'
+      }
+    }
+  }
+    
 
   });
 
